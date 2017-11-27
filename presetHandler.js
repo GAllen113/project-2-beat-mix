d53277320a3100e412e2706a9e0862cf753c0879
Brendalyn Harper <BrendalynHarper@Brendalyns-MacBook-Pro.local>
2017-11-27 00:17:59 -0500

    B. Harper's first code commits

--- presetHandler.js
+++ presetHandler.js
@@ -2,7 +2,14 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (request, arr, arg) => {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== void 0) {
+      arr.unshift("404"); 
+    }
+   return arr[i];
+  }
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,24 @@
-// Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+// Drum Arrays: length of 16 and filled with false.
+var kicks = new Array(16).fill(false);
+var snares = new Array(16).fill(false);
+var hiHats = new Array(16).fill(false);
+var rideCymbals = new Array(16).fill(false);
+
+//Flip the boolean value in the correct array at the specified index.
+function toggleDrum (arr, arrValue){
+ return !arr[arrValue];
+}
+
+//Sets all values in the correct array to false.
+function clear (arr){
+ return  arr.fill(false);
+}
+
+//Flips the boolean value of all elements in the correct array.
+function invert (arr) {
+  for (var i = 0; i < arr.length; i++) {
+    arr[i] = ! arr[i];
+  }
+  return arr[i];
+}
 
