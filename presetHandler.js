c9f9e4fb9c80bb57276cdfc4511befcd2d8b5e89
Brendalyn Harper <BrendalynHarper@Brendalyns-MacBook-Pro.local>
2017-11-27 10:15:12 -0500

    presetHandler.js update.. Not tested.

--- presetHandler.js
+++ presetHandler.js
@@ -3,12 +3,27 @@ const presets = require('./presets');
 
 // Complete this function:
 const presetHandler = (request, arr, arg) => {
+  
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] !== void 0) {
-      arr.unshift("404"); 
+      return arr.unshift("404");
     }
-   return arr[i];
+    else {
+      return arr.unshift("200");
+    }
+
+  switch(request){
+    case 'GET':
+      //code arg.push(arr[i].item);
+      break;
+    case 'PUT':
+      //code return arr.splice(2, 0, i);
+      break;
+    default:
+      //code return arr.unshift("400");
   }
+}
+    
 };
 
 // Leave this line so that your presetHandler function can be used elsewhere:
